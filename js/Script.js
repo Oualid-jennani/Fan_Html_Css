1e4d7fcc88a986714343072913a47ca64f9c40a3
Oualid Jennani <walid.tdi.jennani@gmail.com>
2021-07-22 22:10:37 +0000

    Initial commit

---
+++ js/Script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    
+    $('.move').click(function(){
+        $('.farfara-head').toggleClass('rotate-div');
+        $('.pl1').toggleClass('on');
+    })
+    
+
+});
+
+
+
+
+
+
